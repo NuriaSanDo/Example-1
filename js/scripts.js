91ceed0c94384a5f549fcca7976d6d6858eb40d1
NurSando <nsanchezd@gmail.com>
2024-03-17 15:36:06 +0100

    add sticky menu on mobile to improve browsing

--- js/scripts.js
+++ js/scripts.js
@@ -67,4 +67,41 @@
         modal = document.querySelector('.modal')
         modal.classList.remove('visible');
     }
+
+
+//Observamos si el menú superior se ve en pantalla
+//// Create a new IntersectionObserver object
+let observer = new IntersectionObserver(function (entries, observer) {
+	console.log('An intersection happened!');
+	for (let entry of entries) {
+		if(!entry.isIntersecting) {
+            document.querySelector('.header__links').classList.add('sticky');
+        } else {
+            document.querySelector('.header__links').classList.remove('sticky');
+        }
+	}
+});
+
+// The element to observe
+let app = document.querySelector('p.header__submit');
+
+// Observe the #app element
+observer.observe(app);
+
+
+
+/// Scroll to function
+document.querySelectorAll('a.header__links--item').forEach( anchor => {
+    anchor.addEventListener('click', event => {
+      let URL = event.target.href.split('#');
+      let targetElement = document.querySelector('#'+URL[1])  
+      event.preventDefault();
+      window.scroll({ top: targetElement.offsetTop-80, left: 0, behavior: 'smooth' });
+    });
+  });
+
+
+
+
+
 })();
